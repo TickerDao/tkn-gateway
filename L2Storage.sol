b4e8866761aa626171f9e04e6a15591349463dfd
Kindnesss.eth <kindnesss@superadditive.io>
2024-01-15 14:41:58 -0600

    Augmented L2 resolver contract with events for subgraph parsing

--- L2Storage.sol
+++ L2Storage.sol
@@ -12,6 +12,11 @@ contract L2Storage is Ownable, IERC165 {
 
 	event AdminChanged(address indexed op, bool indexed admin);
 	event DataChanged(string indexed node, string indexed key, uint256 nonce);
+	
+	event GraphTokenChanged(uint256 indexed tokenId, string indexed key, string indexed value);
+  	event GraphAddressChanged(uint256 indexed tokenId, string indexed key, string indexed value);
+	event GraphTokenBytesChanged(uint256 indexed tokenId, string indexed key, bytes value);
+  	event GraphAddressBytesChanged(uint256 indexed tokenId, string indexed key, bytes value);
 
 	mapping (string => mapping(string => bytes)) _data;
 	mapping (string => uint256) _nonce;
@@ -35,10 +40,25 @@ contract L2Storage is Ownable, IERC165 {
 		uint256 nonce = ++_nonce[node];
 		emit DataChanged(node, key, nonce);
 	}
-	function setString(string calldata node, string memory key, string memory s) external {
+	function setString(string calldata node, string memory key, string memory s) public {
 		setData(node, key, bytes(s));
 	}
-
+	function setDataWithGraphTokenData(string calldata node, string memory key, bytes memory v, uint256 tokenId) external {
+		emit GraphTokenBytesChanged(tokenId, key, v);
+		setData(node, key, v);
+	}
+	function setDataWithGraphAddressData(string calldata node, string memory key, bytes memory v, uint256 tokenId) external {
+		emit GraphAddressBytesChanged(tokenId, key, v);
+		setData(node, key, v);
+	}
+	function setStringWithGraphTokenData(string calldata node, string memory key, string memory s, uint256 tokenId) external {
+		emit GraphTokenChanged(tokenId, key, s);
+		setString(node, key, s);
+	}
+	function setStringWithGraphAddressData(string calldata node, string memory key, string memory s, uint256 tokenId) external {
+		emit GraphAddressChanged(tokenId, key, s);
+		setString(node, key, s);
+	}
 	function getData(string calldata node, string calldata key) external view returns (bytes memory v) {
 		v = _data[node][key];
 	}
@@ -53,5 +73,21 @@ contract L2Storage is Ownable, IERC165 {
 		}
 		nonce = _nonce[node];
 	}
-
+	
+	function multicall(bytes[] calldata calls) external returns (bytes[] memory results) {
+		results = new bytes[](calls.length);
+		for(uint i = 0; i < calls.length; i++) {
+			(bool success, bytes memory result) = address(this).delegatecall(calls[i]);
+			if (!success) {
+				assembly {
+					let ptr := mload(0x40)
+					mstore(ptr, returndatasize())
+					returndatacopy(ptr, 0, returndatasize())
+					revert(ptr, returndatasize())
+				}
+			}
+			results[i] = result;
+		}
+		return results;
+	}
 }
